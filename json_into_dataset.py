3c3aff7ae472a5c82af45c424e7eaf1d5f93ebac
Oleg Ianchenko <oleg.ianch@gmail.com>
2024-02-16 19:34:56 -0800

    kjshdjgkshdfg

--- json_into_dataset.py
+++ json_into_dataset.py
@@ -2,10 +2,4 @@ import datasets
 import json
 
 INPUT_FILE = "./data/commonsense_qa/additional_qa.json"
-
-json_file = None
-
-with open(INPUT_FILE, mode='r') as f:
-    json_file = json.load(f)
-
 dataset = datasets.DatasetDict.from_json(INPUT_FILE)
